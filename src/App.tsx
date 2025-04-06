55a1c2b2057d273f34782e11ac0e195864bd8eba
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-04-06 19:26:19 -0300

    Projeto inicializado

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './Globals.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl'>Projeto Dashboard</h1>
+      <Button>
+        Teste Button
+      </Button>
+    </>
+  )
+}
+
+export default App
